5a016ba4ad83efe9f37a25bc3b35ceea598b180d
eugferman <47825454+eugferman@users.noreply.github.com>
2019-12-24 19:25:44 +0100

    delete config.js because the api data was visible
    add again the config.js firebase to connect the api.

--- src/config.js
+++ src/config.js
@@ -1,14 +1,3 @@
-export default {
-  firebaseConfig : {
-    apiKey: "AIzaSyD917-cQPqC4Ow1cu6gJMLwLxOxkVZrDKQ",
-    authDomain: "robinhooks-eugenio.firebaseapp.com",
-    databaseURL: "https://robinhooks-eugenio.firebaseio.com",
-    projectId: "robinhooks-eugenio",
-    storageBucket: "robinhooks-eugenio.appspot.com",
-    messagingSenderId: "796431814179",
-    appId: "1:796431814179:web:5ea7987ce5a85a7564ab96",
-    measurementId: "G-CTG1Q8BT8Z"
-  }
-}
+
 
 
